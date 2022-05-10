3941d27b4bac0a6c958d0dcb1dabbf7852cd20b4
Weiran Zou <zouweiran9122@gmail.com>
2022-05-10 19:46:58 +1000

    Update server to fetch reduced results from couchdb and respond them

--- server/venv/server.py
+++ server/venv/server.py
@@ -1,6 +1,6 @@
 from flask import Flask, jsonify, abort, request, make_response, url_for
 import couchdb
-couch = couchdb.Server()
+couch = couchdb.Server("http://admin:admin@172.26.130.202:5984")
 db = couch['twitter']
 app = Flask(__name__, static_url_path="")
 
@@ -13,31 +13,47 @@ def not_found(error):
 def not_found(error):
     return make_response(jsonify({'error': 'Not found'}), 404)
 
-@app.route('/area/tweets', methods=['GET'])
-def get_tasks():
-    map_fun = '''function (doc) {    
-          emit(doc.area, 1);      
-      }
-    }'''
-    reduce_fun = ''' function(keys, values) {
-            return sum(values);
-        } '''
-    results = db.query(map_fun, reduce_fun)
-    return jsonify({'results': results})
+@app.route('/food', methods=['GET'])
+def get_food():
+    results1 = db.view('area_sentiement/get_area_sentiement', reduce=True, group_level=1)
+    results2 = db.view('area_sentiement/get_area_sentiement', reduce=True, group_level=2)
+    results3 = db.view('area_sentiement/get_area_sentiement', reduce=True, group_level=3)
+    result = {}
+    tweets_results = []
+    for r in results1:
+        dic = {}
+        dic["area"] = r.key[0]
+        dic["num_tweets"] = r.value
+        tweets_results.append(dic)
+    print(tweets_results)
+    sentiments = []
+    areas = ["Melbourne - Inner", "Melbourne - Inner East", "Melbourne - Inner South","Melbourne - North East","Melbourne - North West",
+             "Melbourne - Outer East","Melbourne - South East","Melbourne - West","Mornington Peninsula"]
+    print('\n')
+    for area in areas:
+        dic = {}
+        dic["area"] = area
+        dic["num_positive"] = 0
+        dic["num_negative"] = 0
+        sentiments.append(dic)
+    for r in results2:
+        for s in sentiments:
 
-@app.route('/area/tweets_attitude', methods=['GET'])
-def get_tasks():
-    map_fun = '''function (doc) {    
-              emit(doc.area, doc.attitude 1);      
-          }
-        }'''
-    reduce_fun = ''' function(keys, values) {
-                return sum(values);
-            } '''
-    results = db.query(map_fun, reduce_fun)
-    return jsonify({'results': results})
+            if r.key[0] == s["area"]:
+                if r.key[1] == "POSITIVE":
+                    s["num_positive"] = r.value
+                else:
+                    s["num_negative"] = r.value
+
+    coordinates = []
+    for r in results3:
+        coordinates.append(r.key[2])
+    result['num_tweets'] = tweets_results
+    result['sentiements'] = sentiments
+    result['coordinates'] = coordinates
+    return jsonify({'results': result})
 
 
-@app.route('/aurin', methods=['GET'])
-def get_tasks():
-    return jsonify({'result': ""})
+
+if __name__ == '__main__':
+    app.run(debug=True)
--- server/venv/twitter_harvester.py
+++ server/venv/twitter_harvester.py
@@ -8,9 +8,6 @@ import json
 import threading
 
 
-
-
-
 def processTweet(tweet):
     if tweet.is_quote_status is True and not tweet.text.startswith("RT"):
         if 'extended_tweet' in tweet._json['retweeted_status']:
@@ -28,7 +25,8 @@ def processTweet(tweet):
         else:
             full_text = tweet._json['text']
 
-    return {"coordinates":tweet.coordinates['coordinates'], "text":full_text}
+    return {"coordinates": tweet.coordinates['coordinates'], "text": full_text}
+
 
 load_dotenv()
 bearer_token = os.getenv("TWITTER_BEARER_TOKEN")
@@ -60,38 +58,39 @@ max_id = None
 search recent 7 days tweets api v1.1
 """
 
-def search_recent():
 
+def search_recent():
     num_geo_tweets = 0
     max_results = 100
     limit = 450
     counter = 0
     resps = []
     # https://developer.twitter.com/en/docs/twitter-api/v1/tweets/search/overview
-    #for i in range(2):
+    # for i in range(2):
     for i in range(1):
 
-        resp = api.search_tweets(q=query_string,count=max_results, geocode="-37.81585,144.96313,150km")
+        resp = api.search_tweets(q=query_string, count=max_results, geocode="-37.81585,144.96313,150km")
         with open("test.json", 'w') as f:
-            for i,tweet in enumerate(resp):
+            for i, tweet in enumerate(resp):
                 if tweet.coordinates is not None:
                     resps.append(processTweet(tweet))
                     num_geo_tweets += 1
                 jsonStr = json.dumps(tweet._json)
-                f.write(jsonStr+"\n")
+                f.write(jsonStr + "\n")
 
-            while(len(resp) > 0):
+            while (len(resp) > 0):
                 max_id = resp[-1].id - 1
-                resp = api.search_tweets(q=query_string,count=max_results, geocode="-37.81585,144.96313,150km",max_id=max_id)
+                resp = api.search_tweets(q=query_string, count=max_results, geocode="-37.81585,144.96313,150km",
+                                         max_id=max_id)
 
                 counter += 1
                 for tweet in resp:
                     if tweet.coordinates is not None:
                         resps.append(processTweet(tweet))
                         num_geo_tweets += 1
-                    #json.dumps(tweet._json, f, indent=2)
+                    # json.dumps(tweet._json, f, indent=2)
                     jsonStr = json.dumps(tweet._json)
-                    f.write(jsonStr+"\n")
+                    f.write(jsonStr + "\n")
                 if (counter == limit):
                     print("sleep")
                     print(num_geo_tweets)
@@ -108,13 +107,14 @@ def search_recent():
 Stream
 """
 
+
 class TweetListener(Stream):
 
     def on_status(self, status):
-        #print(status.id)
+        # print(status.id)
         print(status.text)
-        #print(status.user.id)
-        #print(api.user_timeline(user_id=status.user.id))
+        # print(status.user.id)
+        # print(api.user_timeline(user_id=status.user.id))
 
     def on_request_error(self, status_code):
         print(status_code)
@@ -122,13 +122,15 @@ class TweetListener(Stream):
     def on_connection_error(self):
         self.disconnect()
 
+
 tweetListener = TweetListener(
     os.getenv("CONSUMER_KEY"), os.getenv("CONSUMER_KEY_SECRET"),
     os.getenv("ACCESS_KEY"), os.getenv("ACCESS_KEY_SECRET")
 )
 
+
 def stream():
-    tweetListener.filter(track=["food OR restaurant"], locations=[144.3336,-38.5030,145.8784,-37.1751])
+    tweetListener.filter(track=["food OR restaurant"], locations=[144.3336, -38.5030, 145.8784, -37.1751])
 
 
 def search_30():
@@ -142,7 +144,7 @@ def search_30():
             for tweet in page:
                 if tweet.coordinates is not None:
                     tweets.append(tweet)
-                    #print(tweet.created_at, tweet.text, tweet.coordinates)
+                    # print(tweet.created_at, tweet.text, tweet.coordinates)
             lock.release()
             if counter == limit:
                 print("requests exceed minute limit")
@@ -177,45 +179,45 @@ def search_full(bearerToken, label, fromDate, toDate):
     except:
         print("requests exceed maximum limit")
 
-#Create two threads as follows
-bearer_tokens = ["TWITTER_BEARER_TOKEN", "TWITTER_BEARER_TOKEN2","TWITTER_BEARER_TOKEN3","TWITTER_BEARER_TOKEN4","TWITTER_BEARER_TOKEN5"]
-labels = ["devfull","devleo"]
-fromDates = ["202104080000", "202004080000","201904080000","201804080000","201704080000"]
-toDates = ["202204080000", "202104070000","202004070000""201904070000","201804070000"]
+
+# Create two threads as follows
+bearer_tokens = ["TWITTER_BEARER_TOKEN", "TWITTER_BEARER_TOKEN2", "TWITTER_BEARER_TOKEN3", "TWITTER_BEARER_TOKEN4",
+                 "TWITTER_BEARER_TOKEN5"]
+labels = ["devfull", "devleo"]
+fromDates = ["202104080000", "202004080000", "201904080000", "201804080000", "201704080000"]
+toDates = ["202204080000", "202104070000", "202004070000""201904070000", "201804070000"]
 try:
-   t0 = threading.Thread(target=search_recent) # 7 days
-   t1 = threading.Thread(target=search_30)
-   t2 = threading.Thread(target=stream)
-   threads.append(t0)
-   threads.append(t1)
-   threads.append(t2)
-   t0.start()
-   t1.start()
-   t2.start()
-   for i in range(5):
+    t0 = threading.Thread(target=search_recent)  # 7 days
+    t1 = threading.Thread(target=search_30)
+    t2 = threading.Thread(target=stream)
+    threads.append(t0)
+    threads.append(t1)
+    threads.append(t2)
+    t0.start()
+    t1.start()
+    t2.start()
+    for i in range(5):
         t = threading.Thread(target=search_full, args=[bearer_tokens[i], labels[i], fromDates[i], toDates[i]])
         threads.append(t)
         t.start()
-   [thread.join() for thread in threads]
-   with open("food.js", "a+") as f:
-       for tweet in tweets:
-           jsonStr = json.dumps(tweet._json)
-           f.write(jsonStr + "\n")
-       f.close()
+    [thread.join() for thread in threads]
+    with open("food.js", "a+") as f:
+        for tweet in tweets:
+            jsonStr = json.dumps(tweet._json)
+            f.write(jsonStr + "\n")
+        f.close()
 
-   #_thread.start_new_thread( search_recent, () )
-   #_thread.start_new_thread(search_30, ())
-   # for i in range(4):
-   #     _thread.start_new_thread(bearer_tokens[i], search_full, (labels[i], fromDates[i], toDates[i]))
+    # _thread.start_new_thread( search_recent, () )
+    # _thread.start_new_thread(search_30, ())
+    # for i in range(4):
+    #     _thread.start_new_thread(bearer_tokens[i], search_full, (labels[i], fromDates[i], toDates[i]))
 
-   #_thread.start_new_thread( stream, () )
+    # _thread.start_new_thread( stream, () )
 except:
     print("Error: unable to start thread")
 
 while 1:
     pass
-
-
 
 
 
