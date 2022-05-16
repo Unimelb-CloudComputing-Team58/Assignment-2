93dc3172b1bdd50d806b65c604fb6afdb477b7b7
Sam Zhang <ohana@Ohana-DX13.localdomain>
2022-05-16 20:31:27 +1000

    add team details as header

--- ansible/deploy.sh
+++ ansible/deploy.sh
@@ -1,3 +1,16 @@
 #!/bin/bash
 
+###################################################
+#  University of Melbourne                        #
+#  COMP90024 Cluster and Cloud Computing          #
+#  2022 Semester 1 - Assignment 2                 #
+#                                                 #
+#  Team 58:                                       #
+#  - (Sam)     Bin Zhang     895427  @ Melbourne  #
+#  - (Joe)     Tianhuan Lu   894310  @ Melbourne  #
+#  - (Leo)     Yicong Li    1307323  @ Melbourne  #
+#  - (Peter)   Weiran Zou   1309198  @ Melbourne  #
+#  - (Thomas)  Chenhao Gu   1147534  @ Melbourne  #
+###################################################
+
 . ./openrc.sh; ansible-playbook --ask-become-pass pb-main.yaml
