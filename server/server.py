e8355dec9d6ed51b0ccbf10866a3e30bd038cc3d
Weiran Zou <zouweiran9122@gmail.com>
2022-05-17 02:41:55 +1000

    upload couchdb data and add Team comment

--- server/server.py
+++ server/server.py
@@ -1,3 +1,16 @@
+'''
+University of Melbourne
+COMP90024 Cluster and Cloud Computing
+2022 Semester 1 Assignment 2
+
+Team 58:
+- (Sam)    Bin Zhang,    895427 @ Melbourne
+- (Joe)    Tianhuan Lu,  894310 @ Melbourne
+- (Leo)    Yicong Li,   1307323 @ Melbourne
+- (Peter)  Weiran Zou,  1309198 @ Melbourne
+- (Thomas) Chenhao Gu,  1147534 @ Melbourne
+'''
+
 from flask import Flask, jsonify, abort, request, make_response, url_for, Response
 import couchdb
 import os
--- tweet_harvester/area_coordinate.py
+++ tweet_harvester/area_coordinate.py
@@ -1,3 +1,16 @@
+'''
+University of Melbourne
+COMP90024 Cluster and Cloud Computing
+2022 Semester 1 Assignment 2
+
+Team 58:
+- (Sam)    Bin Zhang,    895427 @ Melbourne
+- (Joe)    Tianhuan Lu,  894310 @ Melbourne
+- (Leo)    Yicong Li,   1307323 @ Melbourne
+- (Peter)  Weiran Zou,  1309198 @ Melbourne
+- (Thomas) Chenhao Gu,  1147534 @ Melbourne
+'''
+
 from math import radians, cos, sin, asin, sqrt, pi
 import json
 import os
